e0cd45c7ed22cb04f5743e2259b4985272429daf
Nicholas Grasevski <grasevski@gmail.com>
2021-09-06 09:17:21 +1000

    Add files

---
+++ Makefile
@@ -0,0 +1,28 @@
+STUDENTID = SID500710654
+
+build/$(STUDENTID)_report.pdf: build/$(STUDENTID)_code.ipynb.pdf build/results.log report/report.tex report/references.bib
+	cd report && pdflatex --synctex=1 report.tex && biber report && pdflatex --synctex=1 report.tex && pdflatex --synctex=1 report.tex
+	cp report/report.pdf $@
+
+build/results.log: assignment1.py venv build
+	mkdir -p Algorithm/Output
+	cd Algorithm && tar xf ../Input.tar.xz
+	venv/bin/python assignment1.py | tee $@
+
+build/$(STUDENTID)_code.ipynb.pdf: build/$(STUDENTID)_code.ipynb
+	venv/bin/jupyter nbconvert --output $(notdir $@) --to pdf $<
+
+build/$(STUDENTID)_code.ipynb: assignment1.py venv build
+	venv/bin/yapf -d $<
+	venv/bin/pycodestyle $<
+	venv/bin/jupytext --output $@ $<
+
+build:
+	mkdir $@
+
+venv: requirements.txt
+	virtualenv $@
+	venv/bin/pip install -r $<
+
+clean:
+	rm -rf Algorithm build/* venv
---
+++ assignment1.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python3
+"""Image classifier tuning and evaluation, performed on a 2018 Macbook Pro."""
+from datetime import datetime
+import h5py
+import json
+import numpy as np
+import pandas as pd
+from typing import Tuple
+from skimage.feature import hog
+from sklearn.ensemble import AdaBoostClassifier, BaggingClassifier
+from sklearn.linear_model import LogisticRegression, SGDClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import GridSearchCV
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.preprocessing import normalize
+from sklearn.svm import LinearSVC, SVC
+from sklearn.tree import DecisionTreeClassifier
+
+
+def load_data() -> Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
+    """Load data into numpy arrays."""
+    with h5py.File('Algorithm/Input/train/images_training.h5', 'r') as H:
+        X_train = np.copy(H['datatrain'])
+    with h5py.File('Algorithm/Input/train/labels_training.h5', 'r') as H:
+        y_train = np.copy(H['labeltrain'])
+    with h5py.File('Algorithm/Input/test/images_testing.h5', 'r') as H:
+        X_test = np.copy(H['datatest'])
+    with h5py.File('Algorithm/Input/test/labels_testing_2000.h5', 'r') as H:
+        y_test_2000 = np.copy(H['labeltest'])
+    return X_train, y_train, X_test, y_test_2000
+
+
+def preprocess(X: np.ndarray) -> np.ndarray:
+    """Do data cleaning, feature extraction etc."""
+    features = [
+        hog(x.reshape(28, 28),
+            orientations=8,
+            pixels_per_cell=(4, 4),
+            cells_per_block=(3, 3),
+            block_norm='L1-sqrt') for x in X
+    ]
+    return np.concatenate((normalize(X), np.array(features)), axis=1)
+
+
+# Define all the ML algorithms to compare.
+ALGORITHMS = {
+    # 'Nearest Neighbor': (KNeighborsClassifier(n_jobs=-1), {}),
+    # 'SGD': (SGDClassifier(n_jobs=-1, random_state=0), {}),
+    # 'SVM': (SVC(random_state=0), {}),
+    # 'SGD': (SGDClassifier(random_state=0), {
+    #     'loss': ['hinge', 'log'],
+    #     'penalty': ['l2', 'l1'],
+    #     'alpha': [0.0001, 0.001, 0.01, 0.1]
+    # }),
+    'Naive Bayes': (GaussianNB(), {}),
+}
+
+# ALGORITHMS = {
+#     'Nearest Neighbor': (KNeighborsClassifier(n_jobs=-1), {}),
+#     'Logistic Regression': (LogisticRegression(random_state=0,
+# solver='saga', n_jobs=-1), {}),
+#     'Naive Bayes': (GaussianNB(), {}),
+#     'Decision Tree': (DecisionTreeClassifier(random_state=0), {
+#         'criterion': ['gini', 'entropy'],
+#         'splitter': ['best', 'random'],
+#         'ccp_alpha': [0, 0.001, 0.01, 0.1]
+#     }),
+#     'Bagging': (BaggingClassifier(n_estimators=16, n_jobs=-1,
+#                                   random_state=0), {}),
+#     'Ada Boost': (AdaBoostClassifier(random_state=0), {
+#         'n_estimators': [1, 2, 4, 8, 16, 32, 64, 128],
+#         'learning_rate': [0.1, 1]
+#     }),
+#     'SVM': (LinearSVC(random_state=0), {
+#         'penalty': ['l1', 'l2'],
+#         'C': [1e-3, 1e-2, 1e-1, 1e0, 1e1, 1e2, 1e3, 1e4]
+#     }),
+# }
+
+# Load and preprocess the data.
+X_train, y_train, X_test, y_test_2000 = load_data()
+X_train, X_test, best_model = preprocess(X_train), preprocess(X_test), None
+
+# Run hyperparam tuning on each respective ML algorithm.
+for name, (algorithm, param_grid) in ALGORITHMS.items():
+    model = GridSearchCV(algorithm,
+                         param_grid,
+                         scoring='accuracy',
+                         n_jobs=-1,
+                         cv=10)
+    model.fit(X_train, y_train)
+    print(
+        json.dumps({
+            'ts': str(datetime.now()),
+            'msg': 'gridsearch',
+            'name': name,
+            'score': model.best_score_,
+            'time_seconds': model.refit_time_,
+            'ix': int(model.best_index_),
+            'params': model.best_params_
+        }))
+    pd.DataFrame(model.cv_results_).to_csv(f'Algorithm/Output/{name}.csv')
+    if best_model is None or model.best_score_ > best_model.best_score_:
+        best_model = model
+
+# Make test predictions using the best model and evaluate accuracy.
+y_test_pred = best_model.predict(X_test)
+score = accuracy_score(y_test_2000, y_test_pred[:2000])
+print(
+    json.dumps({
+        'ts': str(datetime.now()),
+        'msg': 'accuracy',
+        'accuracy': score
+    }))
+
+# Write test predictions to file.
+with h5py.File('Algorithm/Output/predicted_labels.h5', 'w') as H:
+    H.create_dataset('Output', data=y_test_pred)
